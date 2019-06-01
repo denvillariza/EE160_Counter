5e181864ad9e1ec782b7e709f28ba66a32754b05
CDishonoured109 <32019777+CDishonoured109@users.noreply.github.com>
2019-06-02 00:43:46 +0800

    Update EE160_Counter_Assembly_GitHub.asm

--- EE160_Counter_Assembly_GitHub.asm
+++ EE160_Counter_Assembly_GitHub.asm
@@ -70,113 +70,181 @@
     CONFIG  EBTRB = OFF           ; Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in 
 				  ; other blocks)
 
-;;Initialize Locaiton of varibles
-M_ten1	equ 0x00
-M_ten2	equ 0x01
-M_ten3	equ 0x02
-M_ten4	equ 0x03	
-M_ten5	equ 0x04
-M_ten6	equ 0x05
-M_ten7	equ 0x06
-M_ten8	equ 0x07
-M_ten9	equ 0x08	
-MOR	equ 0x09
-Cost	equ 0x0A
-MAX	equ 0x0B
-t_del	equ 0x0C
-T_Money equ 0x0D
-P5	equ 0x0E
-P10	equ 0x0F
-Product	equ 0x10;indicator which product is taken
-;;				  
-Money	equ 0x1C
-B_Stock	equ 0x1D
-B_Cost	equ 0x1E
-A_Stock	equ 0x1F	
-A_Cost  equ 0x20
-ten1	equ 0x21
-ten2	equ 0x22
-ten3	equ 0x23
-ten4	equ 0x24
-ten5	equ 0x25
-ten6	equ 0x26
-ten7	equ 0x27
-ten8	equ 0x28
-ten9	equ 0x29
-Sub_S	equ 0x2A
-DisAB	equ 0x2B
+
+; Setting of variables in register addresses
+count1 equ 0x20
+count2 equ 0x21
+count3 equ 0x22
+checker_s equ 0x23
+upcount equ 0x30
+upmax equ 0x31
+zeroth equ 0x32
+add_del equ 0x33
+;;;;;;number for simplicity
+d0	equ 0x34
+d1	equ 0x35
+d2	equ 0x36
+d3	equ 0x37
+d4	equ 0x38
+d5	equ 0x39
+d6	equ 0x3A
+d7	equ 0x3B
+d8	equ 0x3C
+d9	equ 0x3D
+;;;;; First segment
+f0	equ 0x3E
+f1	equ 0x3F
+f2	equ 0x40
+f3	equ 0x41
+f4	equ 0x42
+f5	equ 0x43
+f6	equ 0x44
+f7	equ 0x45
+f8	equ 0x46
+f9	equ 0x47
+;;;;;  second segmend
+s0	equ 0x48
+s1	equ 0x49
+s2	equ 0x4A
+s3	equ 0x4B
+s4	equ 0x4C
+s5	equ 0x4D
+s6	equ 0x4E
+s7	equ 0x4F
+s8	equ 0x50
+s9	equ 0x51
+;;;;;;;third segment
+t0	equ 0x52
+t1	equ 0x53
+t2	equ 0x54
+t3	equ 0x55
+t4	equ 0x56
+t5	equ 0x57
+t6	equ 0x58
+t7	equ 0x59
+t8	equ 0x5A
+t9	equ 0x5B
+one	equ 0x5C
+ten	equ 0x5D
+minute	equ 0x5E
+;b2	equ 0X5F
+;b1	equ 0x60
+;b0	equ 0x61
+b2	equ 0X1F; TWO of the pin in LATC has no Digital Output Capabilities
+b1	equ 0x1E; thus will use two segments from LATB insteadsss
+b0	equ 0x1D
+	
 ;Setting for Program counter for both start and interrupt
     org 0x0000
     GOTO  Start
     org 0x0008
-    GOTO ISR
+    GOTO ISR; Interrupt failed to work. Possible because of lack of Pull-up or pull-dow resistors
     org 0x0018
     GOTO ISR
+;START of PROGRAM
 Start
-    ;;; Test Money
-    
-    ;;Initialize Variables
-    MOVLW D'5'
-    MOVWF P5
-    MOVLW D'10'
-    MOVWF P10
-    MOVLW D'99'
-    MOVWF MAX
-    MOVLW D'70'
-    MOVWF A_Cost;; Heineken (70 Pesos)
-    MOVLW D'45'
-    MOVWF B_Cost;; Tiger (45 Pesos)
-    MOVLW D'2'
-    MOVWF A_Stock; Each Has 50 Items
-    MOVLW D'95'
-    MOVWF B_Stock;
-    ;MOVLW 0x0f
-    ;IORWF A_Cost, 1, 0
-    MOVLW B'00010000'
-    MOVWF M_ten1
-    MOVLW B'00100000'
-    MOVWF M_ten2
-    MOVLW B'00110000'
-    MOVWF M_ten3
-    MOVLW B'01000000'
-    MOVWF M_ten4
-    MOVLW B'01010000'
-    MOVWF M_ten5
-    MOVLW B'01100000'
-    MOVWF M_ten6
-    MOVLW B'01110000'
-    MOVWF M_ten7
-    MOVLW B'10000000'
-    MOVWF M_ten8
-    MOVLW B'10010000'
-    MOVWF M_ten9
-    ;;
-    MOVLW D'10'
-    MOVWF ten1
-    MOVLW D'20'
-    MOVWF ten2
-    MOVLW D'30'
-    MOVWF ten3
-    MOVLW D'40'
-    MOVWF ten4
-    MOVLW D'50'
-    MOVWF ten5
-    MOVLW D'60'
-    MOVWF ten6
-    MOVLW D'70'
-    MOVWF ten7
-    MOVLW D'80'
-    MOVWF ten8
-    MOVLW D'90'
-    MOVWF ten9
-    ;;; Initialize Most Significant Byte
-    
       ;Initialize Values
-    MOVLW 0x60
-    MOVWF  OSCCON	;SETTING the FREQUENCY See page 32 of PIC18f4550 SPEC Sheet
-     
-    BCF UCON,3;; Enable RC4, RC5 as digital inputs 
-    BSF UCFG,3
+      MOVLW 0x60
+      MOVWF  OSCCON	;SETTING the FREQUENCY See page 32 of PIC18f4550 SPEC Sheet
+      MOVLW 0x09
+      MOVWF	upmax
+   ;;;;;; intiialize values
+      MOVLW 0x00
+      MOVWF d0
+      MOVLW 0x01
+      MOVWF d1
+      MOVLW 0x02
+      MOVWF d2
+      MOVLW 0x03
+      MOVWF d3
+      MOVLW 0x04
+      MOVWF d4
+      MOVLW 0x05
+      MOVWF d5
+      MOVLW 0x06
+      MOVWF d6
+      MOVLW 0x07
+      MOVWF d7
+      MOVLW 0x08
+      MOVWF d8
+      MOVLW 0x09
+      MOVWF d9
+      nop
+      ;;;; 7segment initialize;; first digit same with third digit output
+      MOVLW b'00111111'
+      MOVWF f0
+      MOVLW  b'00000110'
+      MOVWF f1
+      MOVLW b'01011011'
+      MOVWF f2
+      MOVLW b'01001111'
+      MOVWF f3
+      MOVLW b'01100110'
+      MOVWF f4
+      MOVLW b'01101101'
+      MOVWF f5
+      MOVLW b'01111101'
+      MOVWF f6
+      MOVLW b'00000111'
+      MOVWF f7
+      MOVLW b'011111111'
+      MOVWF f8
+      MOVLW b'01100111'
+      MOVWF f9
+      nop
+      ;;;;;;;
+      MOVLW b'01110111'
+      MOVWF s0
+      MOVLW b'00000110'
+      MOVWF s1
+      MOVLW b'10110011'
+      MOVWF s2
+      MOVLW b'10010111'
+      MOVWF s3
+      MOVLW b'11000110'
+      MOVWF s4
+      MOVLW b'11010101'
+      MOVWF s5
+      MOVLW b'11110101'
+      MOVWF s6
+      MOVLW b'00000111'
+      MOVWF s7
+      MOVLW b'11110111'
+      MOVWF s8
+      MOVLW b'11000111'
+      MOVWF s9
+      ;;;;;;;
+      MOVLW b'00111111'
+      MOVWF t0
+      MOVLW  b'00000110'
+      MOVWF t1
+      MOVLW b'01011011'
+      MOVWF t2
+      MOVLW b'01001111'
+      MOVWF t3
+      MOVLW b'01100110'
+      MOVWF t4
+      MOVLW b'01101101'
+      MOVWF t5
+      MOVLW b'01111101'
+      MOVWF t6
+      MOVLW b'00000111'
+      MOVWF t7	 
+      MOVLW b'011111111'
+      MOVWF t8
+      MOVLW b'01100111'
+      MOVWF t9
+      nop
+      MOVLW 0x01
+      MOVWF b1
+      MOVLW 0x03
+      MOVWF b2
+      MOVLW 0x00
+      MOVWF b0
+      nop
+      
+      
+      
     MOVLW   0x00    
     MOVWF   ADCON0  ;AD Converver OFF
       
@@ -189,342 +257,321 @@ Start
     CLRF PORTC
     CLRF PORTD
     CLRF PORTE
-    ;;;;;;;;
-    CLRF LATA
-    CLRF LATB
-    CLRF LATC
-    CLRF LATD
-    CLRF LATE
-    ;;;;;;;
-    
-    MOVLW b'01111100'
-    MOVWF TRISA   ;set all PORTA as OUTPUT
-    CLRF TRISB	;Setting all PORTB as OUTPUT
+    CLRF TRISA   ;set all PORTA as OUTPUT
+    MOVLW b'0001100'
+    MOVWF TRISB	;Setting some ports to IssNPUT
    ; CLRF TRISB   ;set all PORTB as OUTPUT
-    MOVLW b'00110000'
-    MOVWF    TRISC  ; RC4, RC5 are set as digital input by disaling UCON<3> = 0 and UCFG<3> = 1
-    ;SETF TRISC   ;set all PORTA as OUTPUT
+    CLRF TRISC   ;set all PORTA as OUTPUT
     CLRF TRISD   ;set all PORTB as OUTPUT
     CLRF TRISE   ;set all PORTA as OUTPUT
-    ;;;;;;;;;;;
     
-InitVar
-    CLRF Money
-    CLRF T_Money
-    CLRF DisAB
-    CLRF Sub_S
-    CLRF MOR
-    CLRF Product
-    Call A_Digit
-    Call B_Digit
-    Call M_Digit
-Check_Product
-    BTFSC PORTA,2
-    GOTO A_Choose; Choose A product
-    BTFSC PORTA,4
-    GOTO B_Choose; Choose B product
-A_Fall
-    BTFSC PORTA,3
-    Call A_Restock
-B_Fall
-    BTFSC PORTA,5
-    GOTO B_Restock
-    GOTO Check_Product
-;;;;;;;;;;
-Set_Loop
-      MOVLW 0xff
-      MOVWF t_del
-add_delay;debounce Logic
-      DECFSZ t_del
-      GOTO add_delay
-      Return
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;debounce logic for A Restock button
-A_Restock
-    MOVLW D'99'
-    CPFSLT A_Stock
-    Return
-    INCF A_Stock
-    Call A_Digit
-    Call Set_Loop
-wait_A
-      BTFSC PORTA,3
-      GOTO wait_A
-      Return
- ;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;debounce logic for B Restock button
-B_Restock
-    MOVLW D'99'
-    CPFSLT B_Stock
-    GOTO Check_Product
-    INCF B_Stock
-    Call B_Digit
-    Call Set_Loop
-wait_B
-      BTFSC PORTA,5
-      GOTO wait_B
-      GOTO Check_Product
- ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Part fter product is Chosen
-A_Choose
-    MOVLW 0x00; Check if there is still stock
-    CPFSGT A_Stock
-    GOTO Check_Product
-    BSF LATA,0;Proceed for next Step
-    MOVFF A_Cost, Cost
-    ;DECF A_Stock
-    ;Call A_Digit; Decrease only once dispense
-    GOTO Money_Input
-B_Choose
-    MOVLW 0x00; Check if there is still stock
-    CPFSGT B_Stock
-    GOTO Check_Product
-    BSF LATA,1; Proceed for next Step
-    MOVLW 0xff
-    MOVWF Product
-    MOVFF B_Cost, Cost
-    ;DECF B_Stock
-    ;Call B_Digit
-    GOTO Money_Input
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-Money_Input
-    BTFSC PORTA,6
-    CALL ADD5
-    BTFSC PORTC,4
-    CALL ADD10
-    BTFSC PORTC,5
-    GOTO Dispense
-    GOTO Money_Input; Check Input buttons
-Dispense
-    MOVFF Cost, WREG
-    CPFSLT Money
-    GOTO Dec_Dis
-    GOTO Money_Input
-Dec_Dis
-    MOVLW 0xff
-    CPFSEQ Product
-    GOTO DECA
-    GOTO DECB
-DECA
-    DECF A_Stock
-    BCF LATA, 0
-    GOTO InitVar
-DECB
-    DECF B_Stock
-    BCF LATA, 1
-    GOTO InitVar
     
-    ;GOTO InitVars
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-ADD5
-    MOVLW D'5'
-    ADDWF T_Money, 1, 0
-    MOVLW D'100'
-    CPFSLT T_Money
-    GOTO Max_Money5
-    MOVFF T_Money, Money
-    Call M_Digit
-    Call Set_Loop
-wait_5
-    BTFSC PORTA,6
-    GOTO wait_5
-    Return
-Max_Money5
-    MOVLW D'99'
-    MOVWF T_Money
-    MOVWF Money
-    CALL Set_Loop
-    GOTO wait_5
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-ADD10
-    MOVLW D'10'
-    ADDWF T_Money, 1, 0
-    MOVLW D'100'
-    CPFSLT T_Money
-    GOTO Max_Money10
-    MOVFF T_Money, Money
-    Call M_Digit
-    Call Set_Loop
-wait_10
-    BTFSC PORTC,4
-    GOTO wait_10
-    Return
-Max_Money10
-    MOVLW D'99'
-    MOVWF T_Money
-    MOVWF Money
-    Call M_Digit
-    Call Set_Loop
-    GOTO wait_10
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-A_Digit
-    MOVLW 0x00
-    MOVWF Sub_S
-    CLRF  DisAB
-    MOVFF A_Stock, WREG
-    CPFSGT ten1
-    MOVFF ten1, Sub_S
-    CPFSGT ten2
-    MOVFF ten2, Sub_S
-    CPFSGT ten3
-    MOVFF ten3, Sub_S
-    CPFSGT ten4
-    MOVFF ten4, Sub_S
-    CPFSGT ten5
-    MOVFF ten5, Sub_S
-    CPFSGT ten6
-    MOVFF ten6, Sub_S
-    CPFSGT ten7
-    MOVFF ten7, Sub_S
-    CPFSGT ten8
-    MOVFF ten8, Sub_S
-    CPFSGT ten9
-    MOVFF ten9, Sub_S
-    ;MOVLW 0x0A
-    ;MOVWF Sub_S
-    ;MOVLW 0x10
-    BSF STATUS, 0;; set Carry to 1. 
-    SUBFWB Sub_S , 0, 0 ;Cost - tens = first digit
-    MOVWF DisAB
-    ;MOVFF  DisAB, LATB
-    ;; For most significant 4bits
-    MOVFF Sub_S, WREG
-    CPFSGT ten1
-    MOVFF M_ten1, MOR
-    CPFSGT ten2
-    MOVFF M_ten2, MOR
-    CPFSGT ten3
-    MOVFF M_ten3, MOR
-    CPFSGT ten4
-    MOVFF M_ten4, MOR
-    CPFSGT ten5
-    MOVFF M_ten5, MOR
-    CPFSGT ten6
-    MOVFF M_ten6, MOR
-    CPFSGT ten7
-    MOVFF M_ten7, MOR
-    CPFSGT ten8
-    MOVFF M_ten8, MOR
-    CPFSGT ten9
-    MOVFF M_ten9, MOR
-    MOVFF MOR,WREG
-    IORWF DisAB, 1, 0
-    ;MOVFF Sub_S, WREG
-    MOVFF DisAB, LATB
-    RETURN
-    ;MOVLW 0x0f
-    ;IORWF A_Cost, 1, 0
-B_Digit
-    MOVLW 0x00
-    MOVWF Sub_S
-    CLRF MOR
-    MOVFF B_Stock, WREG
-    CPFSGT ten1
-    MOVFF ten1, Sub_S
-    CPFSGT ten2
-    MOVFF ten2, Sub_S
-    CPFSGT ten3
-    MOVFF ten3, Sub_S
-    CPFSGT ten4
-    MOVFF ten4, Sub_S
-    CPFSGT ten5
-    MOVFF ten5, Sub_S
-    CPFSGT ten6
-    MOVFF ten6, Sub_S
-    CPFSGT ten7
-    MOVFF ten7, Sub_S
-    CPFSGT ten8
-    MOVFF ten8, Sub_S
-    CPFSGT ten9
-    MOVFF ten9, Sub_S
-    BSF STATUS, 0;; set Carry to 1. 
-    SUBFWB Sub_S , 0, 0 ;Cost - tens = first digit
-    MOVWF DisAB
-    ;MOVFF  DisAB, LATD
-    MOVFF Sub_S, WREG
-    CPFSGT ten1
-    MOVFF M_ten1, MOR
-    CPFSGT ten2
-    MOVFF M_ten2, MOR
-    CPFSGT ten3
-    MOVFF M_ten3, MOR
-    CPFSGT ten4
-    MOVFF M_ten4, MOR
-    CPFSGT ten5
-    MOVFF M_ten5, MOR
-    CPFSGT ten6
-    MOVFF M_ten6, MOR
-    CPFSGT ten7
-    MOVFF M_ten7, MOR
-    CPFSGT ten8
-    MOVFF M_ten8, MOR
-    CPFSGT ten9
-    MOVFF M_ten9, MOR
-    MOVFF MOR,WREG
-    IORWF DisAB, 1, 0
-    MOVFF DisAB, LATD
-    RETURN
-M_Digit
-    MOVLW 0x00
-    MOVWF Sub_S
-    CLRF MOR
-    MOVFF Money, WREG
-    CPFSGT ten1
-    MOVFF ten1, Sub_S
-    CPFSGT ten2
-    MOVFF ten2, Sub_S
-    CPFSGT ten3
-    MOVFF ten3, Sub_S
-    CPFSGT ten4
-    MOVFF ten4, Sub_S
-    CPFSGT ten5
-    MOVFF ten5, Sub_S
-    CPFSGT ten6
-    MOVFF ten6, Sub_S
-    CPFSGT ten7
-    MOVFF ten7, Sub_S
-    CPFSGT ten8
-    MOVFF ten8, Sub_S
-    CPFSGT ten9
-    MOVFF ten9, Sub_S
-    BSF STATUS, 0;; set Carry to 1. 
-    SUBFWB Sub_S , 0, 0 ;Cost - tens = first digit
-    MOVWF DisAB
-    BTFSC DisAB,3
-    BSF DisAB, 6
-    ;;;
-    MOVFF Sub_S, WREG
-    CPFSGT ten1
-    MOVFF M_ten1, MOR
-    CPFSGT ten2
-    MOVFF M_ten2, MOR
-    CPFSGT ten3
-    MOVFF M_ten3, MOR
-    CPFSGT ten4
-    MOVFF M_ten4, MOR
-    CPFSGT ten5
-    MOVFF M_ten5, MOR
-    CPFSGT ten6
-    MOVFF M_ten6, MOR
-    CPFSGT ten7
-    MOVFF M_ten7, MOR
-    CPFSGT ten8
-    MOVFF M_ten8, MOR
-    CPFSGT ten9
-    MOVFF M_ten9, MOR
-    BTFSC MOR,4
-    BSF DisAB, 7
-    MOVFF DisAB, LATC
-    CLRF LATE
-    BTFSC MOR,5
-    BSF LATE, 0
-    BTFSC MOR,6
-    BSF LATE, 1
-    BTFSC MOR,7
-    BSF LATE,2
-    Return
     
+
+    
+   ; BSF TRISB, 0   ;set RB0 as input
+  ;  BSF TRISB, 1
+  ;MOVLW b'01000000'
+   ; MOVWF   INTCON2
+
+    MOVLW 0x00
+    nop
+    CALL second_digit	;Function cll to display numbers
+    nop
+    CALL first_digit
+    CALL first_digit
+    nop
+    CALL third_digit
+    nop
+    CALL first_digit
+    MOVWF one
+    MOVWF ten
+    MOVWF checker_s
+    GOTO Init
 ISR
     MOVLW b'01000001'
     MOVWF LATA
-    GOTO ISR 
+    GOTO ISR
+Init
+    ;CLRF LATA
+    CLRF zeroth
+    CLRF upcount
+    MOVLW 0x09
+    MOVWF upmax
     
-END
+check_up    ;Check if Up button is pressed
+    BTFSS PORTB,2
+    GOTO check_start
+    GOTO add_check
+ 
+check_start ;check if Start Button is pressed
+     BTFSS PORTB,3
+     GOTO check_up
+     GOTO  cstart
+     
+add_check;;;;;;
+     MOVFF upcount,WREG
+     CPFSEQ upmax ;check if equal to 9
+     GOTO add
+     GOTO check_start
+add
+      INCF	upcount
+      MOVFF upcount,WREG
+      ;MOVFF WREG,LATA
+     CALL first_digit;;;;; convert to seven segment
+     nop
+fback 
+      MOVLW 0xff
+      MOVWF add_del
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;      
+add_delay;debounce Logic
+      DECFSZ add_del
+      GOTO add_delay
+wait_fall
+      BTFSC PORTB,2
+      GOTO wait_fall
+      GOTO check_start
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+cstart
+      MOVFF upcount,WREG
+      CPFSLT zeroth;;if upount>zeroth skip
+      GOTO check_up
+      GOTO  loop1;initial
+   
+ten_set
+    MOVLW 0x05
+    MOVWF ten
+one_set
+    movlw 0x09
+    MOVWF one   
+loop1
+;;;; Algorithm for loop1
+    ;1. Display First All Values
+    ;2. Decrement 'one'
+    ;3. Check if the counter is now only at 10 seconds
+    ;4. Delay for 1 second
+    ;5. Check if boolean from # 3 is true of false
+    ;5a.    If yes, proceed to mili Algorithmm
+    ;5b.    Otherwise, continue loop1 algorithm
+    ;6.	Check if 'on'e value is 255.(if a value of zero is decremented, the resulting value will be 255.)
+    ;6a.    If yes, and tens value will be deducted
+    ;6b.    Otherwise, restart loop1 Algorithm, If the previous condition is true
+    ;7	 Check if 'tens' reached 255
+    ;7a	    If yes, 'tens' and 'ones' value will be reinitaized and value of 'upcount'(minute) will be deducted
+    ;7b	    If no, continue, the ones value will only be reinitailized
+    MOVFF upcount,WREG	;1
+    nop
+    call first_digit
+    call first_digit
+    call first_digit
+    MOVFF ten,WREG
+    call second_digit
+    MOVFF one,WREG
+    call third_digit
+ ;;;;;;;;;;
+    decf one	    ;2
+    CALL check_shift	;3
+    ;;;;Delay
+    Call s1_set	;4
+    ;;;;;;;
+    
+    movlw 0x00
+    CPFSEQ checker_s	;5
+    GOTO mili	;5a
+    ;;;;;;;;
+    movlw 0xff	;5b
+    CPFSEQ one	;6b
+    GOTO loop1	;
+    
+    decf ten	;6a
+   ;;;initial
+    
+    ;;; insert if statement
+    movlw 0xff
+    CPFSEQ ten	;7
+    GOTO one_set    ;7b
+    decf upcount    ;7a
+    
+    GOTO ten_set
+check_shift; Checking if one = 0, minute = 0, and tens = 0
+      movlw 0x00
+      CPFSEQ upcount
+      Return
+       movlw 0x00
+      CPFSEQ one
+      Return
+      MOVLW 0x01
+      CPFSEQ ten
+      Return
+      SETF checker_s
+      Return
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Delay for 1 s// recommend using online delay generator
+s1_set; 
+     movlw	0xce
+     movwf	count1
+      movlw	0x91
+      movwf	count2
+      movlw	0x05
+      movwf	count3
+Delay1
+     decfsz	count1, f
+    goto	Delay1
+    decfsz	count2, f
+    goto	Delay1
+    decfsz	count3, f
+    goto	Delay1
+    nop
+    Return
+    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Delay for 10 ms
+s2_set
+     movlw	0xad
+     movwf	count1
+      movlw	0x0c
+      movwf	count2
+
+Delay2
+     decfsz	count1, f
+	goto	Delay2
+	decfsz	count2, f
+	goto	Delay2
+    Return
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+mili
+      MOVLW 0x09
+    MOVWF upcount; for debuggin purposes
+ten_setm
+    MOVLW 0x09
+    MOVWF ten
+one_setm
+    movlw 0x09
+    MOVWF one   
+loop2
+;;;; Algorithm for loop2
+    ;1. Display First All Values
+    ;2. Decrement 'one'
+    ;3. Check if 'one' is 255
+    ;3a.    If yes, 'tens' will be deducted
+    ;3b.    Otherwise, call loop2 algorithm again
+    ;4.	Check if 'ten' value is 255.
+    ;4a.    If yes, 'upstart'(now the second) value will be deducted'
+    ;4b.    Otherwise, the value of 'one' will be reinitialized
+    ;5	 Check if upstart reached 255
+    ;5a	    If yes, the code will reset itself
+    ;5b	    If no, both value of 'tens' and ones' will be reinitialized
+    MOVFF upcount,WREG	;1
+    nop
+    call first_digit
+    call first_digit
+    call first_digit
+    MOVFF ten,WREG
+    call second_digit
+    MOVFF one,WREG
+    call third_digit
+    ;;;;;;;;;;;;;;
+    decf one		;2
+    
+    ;;;;Delay
+    Call s2_set		;3
+    ;;
+    movlw 0xff
+    CPFSEQ one
+    GOTO loop2		;3b
+    decf ten		;3a
+    CPFSEQ ten		;4
+    GOTO one_setm	;4b
+    decf upcount	;4a
+    CPFSEQ upcount	;5
+    GOTO ten_setm	;5b
+     Reset		;5a
+      
+      
+      ;;;;;;;;;;;;;;;;;
+first_digit
+     CPFSLT d9
+      MOVFF t9, LATD
+     CPFSLT  d8
+      MOVFF t8, LATD
+     CPFSLT  d7
+      MOVFF t7, LATD
+     CPFSLT  d6
+      MOVFF t6, LATD
+     CPFSLT  d5
+      MOVFF t5, LATD
+     CPFSLT  d4
+      MOVFF t4, LATD
+     CPFSLT  d3
+      MOVFF t3, LATD
+     CPFSLT  d2
+      MOVFF t2, LATD
+     CPFSLT  d1
+      MOVFF t1, LATD
+     CPFSLT  d0
+      MOVFF t0, LATD
+     RETURN
+second_digit
+     CPFSLT d9
+      MOVFF s9, LATC
+     CPFSLT  d8
+      MOVFF s8, LATC
+     CPFSLT  d7
+      MOVFF s7, LATC
+     CPFSLT  d6
+      MOVFF s6, LATC
+     CPFSLT  d5
+      MOVFF s5, LATC
+     CPFSLT  d4
+      MOVFF s4, LATC
+     CPFSLT  d3
+      MOVFF s3, LATC
+     CPFSLT  d2
+      MOVFF s2, LATC
+     CPFSLT  d1
+      MOVFF s1, LATC
+     CPFSLT  d0
+      MOVFF s0, LATC
+      
+     CPFSLT  d9
+      MOVFF b0, LATB
+     CPFSLT  d8
+      MOVFF b2, LATB
+     CPFSLT  d7
+      MOVFF b0, LATB
+     CPFSLT  d6
+      MOVFF b2, LATB
+     CPFSLT  d5
+      MOVFF b1, LATB
+     CPFSLT  d4
+      MOVFF b0,LATB
+     CPFSLT  d3
+      MOVFF b1, LATB
+     CPFSLT  d2
+      MOVFF b2, LATB
+     CPFSLT  d1
+      MOVFF b0, LATB
+     CPFSLT  d0
+      MOVFF b2, LATB
+     RETURN
+third_digit
+     CPFSLT d9
+      MOVFF f9, LATA
+     CPFSLT  d8
+      MOVFF f8, LATA
+     CPFSLT  d7
+      MOVFF f7, LATA
+     CPFSLT  d6
+      MOVFF f6, LATA
+     CPFSLT  d5
+      MOVFF f5, LATA
+     CPFSLT  d4
+      MOVFF f4, LATA
+     CPFSLT  d3
+      MOVFF f3, LATA
+     CPFSLT  d2
+      MOVFF f2, LATA
+     CPFSLT  d1
+      MOVFF f1, LATA
+      CPFSLT  d0
+      MOVFF f0, LATA
+     RETURN
+    END
